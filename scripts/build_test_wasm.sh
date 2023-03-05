fe71d319e09e4be1882079f4d8243eb5b60f4d96
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2023-03-05 20:08:45 -0300

    Change the output folder

--- scripts/build_test_wasm.sh
+++ scripts/build_test_wasm.sh
@@ -5,8 +5,8 @@ klu_path=$current_path"/libklusolve"
 lib_path=$klu_path"/lib"
 header_path=$klu_path"/include"
 
-emcc -I$header_path -sMAIN_MODULE test_wasm/main.c $lib_path/libklusolve.so -Wall -Oz -o test_wasm/main.js
-cp $lib_path/libklusolve.so test_wasm/libklusolve.so
+emcc -I$header_path -sMAIN_MODULE test_wasm/main.c $lib_path/libklusolve.so -Wall -Oz -o dist/main.js
+cp $lib_path/libklusolve.so dist/libklusolve.so
 
 # echo "Output:"
 # node Test_Felipe/main.js
