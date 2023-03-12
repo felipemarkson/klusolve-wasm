62ffb7796333fd6663b5da02e9762cbfe2f5a8fe
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2023-03-12 14:35:30 -0300

    Change the WASM output directory

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -1,12 +1,12 @@
 rm -rf build
 rm -rf Lib
-rm -rf libklusolve
+rm -rf libklusolve_wasm
 rm -r Test/KLD*
 
 mkdir build
 cd build
 emcmake cmake   -DCMAKE_BUILD_TYPE=Release \
-                -DCMAKE_INSTALL_PREFIX=../libklusolve \
+                -DCMAKE_INSTALL_PREFIX=../libklusolve_wasm \
                 -DCMAKE_PROJECT_INCLUDE=wasm.cmake \
                 ..
 emmake make
